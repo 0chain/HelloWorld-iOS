ed71b3916c1de28aff5b752762ad6cd087f96ff7
Aaryan Kothari <aaryan.kothari@gmail.com>
2022-12-28 19:03:02 +0530

    Initial Commit

---
+++ ZusExample/ZusExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZusExampleApp.swift
+//  ZusExample
+//
+//  Created by Aaryan Kothari on 28/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ZusExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
