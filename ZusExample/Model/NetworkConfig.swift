24eae90a738c456a925a93a57005561945a6ccb6
Aaryan Kothari <aaryan.kothari@gmail.com>
2022-12-28 19:11:34 +0530

    init: ViewModels

---
+++ ZusExample/Model/NetworkConfig.swift
@@ -0,0 +1,44 @@
+//
+//  NetworkConfig.swift
+//  ZusExample
+//
+//  Created by Aaryan Kothari on 28/12/22.
+//
+
+import Foundation
+
+struct NetworkConfig: Codable {
+    
+    let signatureScheme: String
+    let confirmationChainLength: Int
+    let blockWorker, zboxURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case signatureScheme = "signature_scheme"
+        case confirmationChainLength = "confirmation_chain_length"
+        case blockWorker = "block_worker"
+        case zboxURL
+    }
+    
+    internal init(signatureScheme: String = "bls0chain",
+                  confirmationChainLength: Int = 3,
+                  blockWorker: String, zboxURL: String) {
+        self.signatureScheme = signatureScheme
+        self.confirmationChainLength = confirmationChainLength
+        self.blockWorker = blockWorker
+        self.zboxURL = zboxURL
+    }
+    
+    static let demoZus = NetworkConfig(blockWorker: "https://demo.zus.network/dns", zboxURL: "https://0box.demo.zus.network")
+    static let bcv1 = NetworkConfig(blockWorker: "https://bcv1.devnet-0chain.net/dns", zboxURL: "https://0box.bcv1.devnet-0chain.net")
+    static let demo = NetworkConfig(blockWorker: "https://dev.0chain.net/dns", zboxURL: "https://0box.dev.0chain.net")
+    static let potato = NetworkConfig(blockWorker: "https://potato.devnet-0chain.net/dns", zboxURL: "https://0box.potato.devnet-0chain.net")
+    static let test = NetworkConfig(blockWorker: "https://test.0chain.net/dns", zboxURL: "https://0box.test.0chain.net")
+    
+    var host: String {
+        return URLComponents(string: blockWorker)?.host ?? ""
+    }
+    
+}
+
+
