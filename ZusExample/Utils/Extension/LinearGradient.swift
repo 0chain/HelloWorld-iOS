03f8ae4a65e6c829ba07e3305fee4fe08adeaa1d
Aaryan Kothari <aaryan.kothari@gmail.com>
2023-04-27 18:14:33 +0530

    feat: button gradients

---
+++ ZusExample/Utils/Extension/LinearGradient.swift
@@ -0,0 +1,13 @@
+//
+//  LinearGradient.swift
+//  ZusExample
+//
+//  Created by Aaryan Kothari on 27/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension LinearGradient {
+    static let blue = LinearGradient(colors: [Color("lightBlue"), Color("darkBlue")], startPoint: .topLeading, endPoint: .bottomTrailing)
+}
